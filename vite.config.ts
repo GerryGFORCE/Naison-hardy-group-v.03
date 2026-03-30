e39082d3b3b8c7fb829c01994f5e46cd10e8803b
GerryGFORCE <geraldchauma@gmail.com>
2026-03-31 00:03:12 +0200

    v1

--- vite.config.ts
+++ vite.config.ts
@@ -5,11 +5,19 @@ import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import viteReact from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
-import { nitro } from 'nitro/vite'
-
 
 const config = defineConfig({
-  plugins: [devtools(), tsconfigPaths({ projects: ["./tsconfig.json"] }), tailwindcss(), tanstackStart(), nitro(), viteReact()],
+  plugins: [
+    devtools(),
+    tsconfigPaths({ projects: ["./tsconfig.json"] }),
+    tailwindcss(),
+    tanstackStart({
+      server: {
+        preset: "vercel",
+      },
+    }),
+    viteReact(),
+  ],
   resolve: {
     alias: {
       "#": path.resolve(import.meta.dirname, "./src"),
